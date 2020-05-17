f0102469caaacde16b182e448d4b0cb07bdae4c5
Bui Xuan huynh <huynhxuanbui@gmail.com>
2020-05-17 17:23:02 +0700

    get list product

---
+++ src/dao/ProductDAO.java
@@ -0,0 +1,80 @@
+package dao;
+
+import model.DBConnect;
+import model.Product;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDAO implements IProductDAO {
+    @Override
+    public List<Product> getListProduct() throws SQLException {
+        Connection connection = DBConnect.getConnection();
+        String sql = "select * from product";
+        PreparedStatement ps = connection.prepareCall(sql);
+        ResultSet rs = ps.executeQuery();
+        ArrayList<Product> products = new ArrayList<>();
+        while (rs.next()) {
+            Product product = new Product();
+            product.setProductId(rs.getString("ProductID"));
+            product.setCategoryId(rs.getString("CategoryId"));
+            product.setProductName(rs.getString("ProductName"));
+            product.setProductPrice(rs.getFloat("ProductPrice"));
+            product.setQuantityInStock(rs.getInt("QuantityInStock"));
+            product.setImage(rs.getString("Image"));
+            product.setStatus(rs.getInt("Status"));
+            product.setDescription(rs.getString("Description"));
+            products.add(product);
+        }
+        return products;
+    }
+
+    @Override
+    public List<Product> getListProductIP() throws SQLException {
+        Connection connection = DBConnect.getConnection();
+        String sql = "select product.* from product where ProductID like '%IP%' limit 3";
+        PreparedStatement ps = connection.prepareCall(sql);
+        ResultSet rs = ps.executeQuery();
+        ArrayList<Product> products = new ArrayList<>();
+        while (rs.next()) {
+            Product product = new Product();
+            product.setProductId(rs.getString("ProductID"));
+            product.setCategoryId(rs.getString("CategoryId"));
+            product.setProductName(rs.getString("ProductName"));
+            product.setProductPrice(rs.getFloat("ProductPrice"));
+            product.setQuantityInStock(rs.getInt("QuantityInStock"));
+            product.setImage(rs.getString("Image"));
+            product.setStatus(rs.getInt("Status"));
+            product.setDescription(rs.getString("Description"));
+            products.add(product);
+        }
+        return products;
+    }
+
+    @Override
+    public List<Product> getListProductSS() throws SQLException {
+        Connection connection = DBConnect.getConnection();
+        String sql = "\n" +
+                "select product.* from product where ProductID like '%SS%' limit 3";
+        PreparedStatement ps = connection.prepareCall(sql);
+        ResultSet rs = ps.executeQuery();
+        ArrayList<Product> products = new ArrayList<>();
+        while (rs.next()) {
+            Product product = new Product();
+            product.setProductId(rs.getString("ProductID"));
+            product.setCategoryId(rs.getString("CategoryId"));
+            product.setProductName(rs.getString("ProductName"));
+            product.setProductPrice(rs.getFloat("ProductPrice"));
+            product.setQuantityInStock(rs.getInt("QuantityInStock"));
+            product.setImage(rs.getString("Image"));
+            product.setStatus(rs.getInt("Status"));
+            product.setDescription(rs.getString("Description"));
+            products.add(product);
+        }
+        return products;
+    }
+}
