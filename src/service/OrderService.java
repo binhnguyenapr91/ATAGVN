1408325544aeb4fdd2d802dc7fd966ec802bc7b4
thinhbui2502 <buiducthinh2502@gmail.com>
2020-05-24 22:20:37 +0700

    add crud order

--- src/service/OrderService.java
+++ src/service/OrderService.java
@@ -12,4 +12,14 @@ public interface OrderService {
     void addOrderFromCart(Order order);
     void addOrderProductFromCart(String orderId, String productId, int quantity, float priceEach, String accountId);
     void updateQuantityProduct(int quantity, String productId);
+
+    List<Order> viewAllOrder();
+
+    void updateOder(String orderID, String accountID, String orderDate, String receiver, String address, String email, String phoneNumber, int status);
+
+    void deleteOder(String orderID);
+
+    void deleteOder_product(String orderID);
+
+    Order findByID(String orderIDToFind);
 }
